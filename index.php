abb30efa9602fd951cfcdeb0a5965dde9147498d
kiera91 <kforeilly7@hotmail.com>
2014-09-01 22:25:53 +0100

    got jquery slider in

--- index.php
+++ index.php
@@ -4,7 +4,17 @@
 	
 	<body>
 		<?php include ('header.php'); ?>
-
+		
+		<div id="content">
+			<div id="slider1_container">
+				<!-- Slides Container -->
+				<div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 690px; height: 190px;">
+    				<div><img u="image" src="images/montage1.jpg" /></div>
+    				<div><img u="image" src="images/montage2.jpg" /></div>
+    				<div><img u="image" src="images/montage3.jpg" /></div>
+				</div>
+			</div>
+		</div>	
 		
 		<?php include ('footer.php'); ?>
 
--- js/formbase_furniture.js
+++ js/formbase_furniture.js
@@ -5,4 +5,6 @@ $(document).ready(function() {
 		$("#accordion").accordion({ header: "h4", active: false, autoHeight: false, collapsible: true });
 	});
 
+	var options = { $AutoPlay: true };
+    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
 });
