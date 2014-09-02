7779c5a7c9b260900b87be513631b55d14a1de35
kiera91 <kforeilly7@hotmail.com>
2014-09-02 19:02:23 +0100

    separate sections

---
+++ carousel.php
@@ -0,0 +1,8 @@
+<div id="slider1_container">
+	<!-- Slides Container -->
+	<div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 690px; height: 190px;">
+		<div><img u="image" src="images/montage1.jpg" /></div>
+		<div><img u="image" src="images/montage2.jpg" /></div>
+		<div><img u="image" src="images/montage3.jpg" /></div>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -5,16 +5,9 @@
 	<body>
 		<?php include ('header.php'); ?>
 		
-		<div id="content">
-			<div id="slider1_container">
-				<!-- Slides Container -->
-				<div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 690px; height: 190px;">
-    				<div><img u="image" src="images/montage1.jpg" /></div>
-    				<div><img u="image" src="images/montage2.jpg" /></div>
-    				<div><img u="image" src="images/montage3.jpg" /></div>
-				</div>
-			</div>
-		</div>	
+		<?php include ('sidebar.php'); ?>
+		
+		<?php include ('carousel.php'); ?>
 		
 		<?php include ('footer.php'); ?>
 
