ec00f7ae668cb66180e63df0d6d3321ae0d60b81
kiera91 <kforeilly7@hotmail.com>
2014-09-03 21:46:13 +0100

    redid carousel

--- carousel.php
+++ carousel.php
@@ -1,8 +1,21 @@
-<div id="slider1_container">
-	<!-- Slides Container -->
+<div id="slideshow-container">
+		<div id="slideshow">
+    	    <div class="slide">
+    	    	<img u="image" src="images/montage1.jpg" />
+    	    </div>
+    	    <div class="slide">
+    	    	<img u="image" src="images/montage2.jpg" />
+    	    </div>
+    	    <div class="slide">
+    	    	<img u="image" src="images/montage3.jpg" />
+    	    </div>
+    	</div>
+</div>
+
+<!-- <div id="slider1_container">
 	<div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 690px; height: 190px;">
 		<div><img u="image" src="images/montage1.jpg" /></div>
 		<div><img u="image" src="images/montage2.jpg" /></div>
 		<div><img u="image" src="images/montage3.jpg" /></div>
 	</div>
-</div>
+</div> -->
--- js/formbase_furniture.js
+++ js/formbase_furniture.js
@@ -1,10 +1,45 @@
+var currentSlide = 0;
+var slideWidth;
+var slides;
+var noSlides;
+
 $(document).ready(function() {
 	
 
 	$(function(){
 		$("#accordion").accordion({ header: "h4", active: false, autoHeight: false, collapsible: true });
 	});
-
-	var options = { $AutoPlay: true };
-    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+	
+	workSlideShow();
+	
+	setInterval(changePosition,3000);
+	
+	function workSlideShow() {
+        slideWidth = 690;
+        slides = $('.slide');
+        noSlides = slides.length;
+		        
+        slides.wrapAll('<div id="slidesHolder"></div>');
+        slides.css({
+            'float' : 'left'
+        });
+        $('#slidesHolder').css('width', slideWidth * noSlides);
+	}
+	
+	function changePosition() {
+	            if (currentSlide == noSlides - 1) {
+	                currentSlide = 0;
+	            } else {
+	                currentSlide++;
+	            }
+	            switchSlide();
+	        }
+	
+			/* Switches the slides using animate. */
+	        function switchSlide() {
+	            $('#slidesHolder').animate({
+	                'marginLeft' : slideWidth * (-currentSlide)
+	            });
+	        }
+	
 });
